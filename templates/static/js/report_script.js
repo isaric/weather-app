e7a5be9efb8bc05e345c748a5229fd12b6e0993d
Ivan Šarić <ivan@path-variable.com>
2025-07-06 16:07:14 +0200

    junie styling v2 (#10)
    * junie styling v2
    
    * add take_screenshots.py for capturing automated screenshots of app pages

--- templates/static/js/report_script.js
+++ templates/static/js/report_script.js
@@ -9,3 +9,21 @@ window.onload = () => {
     }).addTo(map);
     let marker = L.marker([lat, lng]).addTo(map);
  }
+// Detect keyboard navigation for accessibility focus styles
+function handleFirstTab(e) {
+    if (e.keyCode === 9) { // Tab key
+        document.body.classList.add('using-keyboard');
+        window.removeEventListener('keydown', handleFirstTab);
+        window.addEventListener('mousedown', handleMouseDownOnce);
+    }
+}
+
+function handleMouseDownOnce() {
+    document.body.classList.remove('using-keyboard');
+    window.removeEventListener('mousedown', handleMouseDownOnce);
+    window.addEventListener('keydown', handleFirstTab);
+}
+
+window.addEventListener('keydown', handleFirstTab);
+
+// Rest of your report_script.js code will go here
